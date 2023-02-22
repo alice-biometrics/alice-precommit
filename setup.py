7475b5624c5d95d06676111184b48d7d2e2f91b7
Fran García Salomón <frangarciasalomon@gmail.com>
2023-02-22 08:24:58 +0100

    chore: create a buildable package

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
-from __future__ import annotations
-
-import argparse
-import re
-from typing import Sequence
+from setuptools import find_packages
 from setuptools import setup
-setup()
+
+
+setup(
+    name='alice-precommit',
+    description='Pre-commit hooks created by Alice Biometrics',
+    url='https://github.com/alicebiometrics/alice-precommit',
+
+    author='Alice Biometrics',
+    entry_points={
+    'console_scripts': [
+        'jwt-checker = hooks.jwt_checker:main'
+    ]
+},
+
+    packages=find_packages()
+)
