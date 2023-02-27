c0b62f366bfbdfe8b807ce79d2b7665663dc6ff6
Fran García Salomón <fgarcia@alicebiometrics.com>
2023-02-27 09:06:30 +0100

    fix: make regex more restrictive (#2)

--- hooks/jwt_checker.py
+++ hooks/jwt_checker.py
@@ -5,7 +5,7 @@ import re
 from typing import Sequence
 
 BLACKLIST_REGEX = [
-    re.compile(b"ey.*\\..+"),
+    re.compile(b"ey\\w*\\..+"),
 ]
 
 
@@ -14,18 +14,18 @@ def main(argv: Sequence[str] | None = None) -> int:
     parser.add_argument("filenames", nargs="*", help="Filenames to check")
     args = parser.parse_args(argv)
 
-    private_key_files = []
+    files_with_jwt = []
 
     for filename in args.filenames:
         with open(filename, "rb") as f:
             content = f.read()
             for match in re.finditer(BLACKLIST_REGEX[0], content):
                 line = content[:match.start()].count(b"\n") + 1
-                private_key_files.append((filename, line))
+                files_with_jwt.append((filename, line))
 
-    if private_key_files:
-        for filename, line in private_key_files:
-            print(f"Private key found in {filename}, line {line}")
+    if files_with_jwt:
+        for file_with_jwt, line in files_with_jwt:
+            print(f"JWT found in {file_with_jwt}, line {line}")
         return 1
     else:
         return 0
